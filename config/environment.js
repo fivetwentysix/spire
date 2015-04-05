ba7f7abc44edc047f04bc8a3d56fa1492b4c82e9
Patrick Ma <patrick.ma@newcontext.com>
2015-04-05 01:11:13 +0000

    relax content security policy

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,11 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    contentSecurityPolicy: {
+      // I dev remotely cant be bothered to care.
+      'connect-src': '*',
+      'script-src': '*'
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
